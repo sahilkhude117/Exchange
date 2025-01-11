e2ab4dbbcb35ece31477b7c180ddae3c3cffede0
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-11 17:05:19 +0530

    added routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import { orderRouter } from "./routes/order";
 import { depthRouter } from "./routes/depth";
+import { tradesRouter } from "./routes/trades";
+import { klineRouter } from "./routes/kline";
+import { tickersRouter } from "./routes/ticker";
 
 const app = express();
 app.use(cors());
@@ -10,6 +13,9 @@ app.use(express.json());
 //Routes
 app.use("/api/v1/order", orderRouter);
 app.use("/api/v1/depth", depthRouter);
+app.use("/api/v1/trades", tradesRouter);
+app.use("/api/v1/klines", klineRouter);
+app.use("/api/v1/tickers", tickersRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
